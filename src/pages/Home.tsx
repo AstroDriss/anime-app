df03267ec64d1a463224973fbdf3f01811c9e482
Idriss Douiri <drisspennywise@gmail.com>
2024-01-26 11:40:32 +0100

    fixing gap problem

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import Trending from "../components/Trending";
 export const Home = () => {
   return (
     <main>
-      <div className="md:grid grid-cols-3 gap-4 mt-7 mb-5">
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-4 mt-7 mb-5">
         <Recommendation className="col-span-2" />
         <Trending />
       </div>
