7f2f7c28d77d5440d4f3b35aab734661e8935a3d
Idriss Douiri <drisspennywise@gmail.com>
2024-01-29 20:39:40 +0100

    Styling the anime card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,18 +10,20 @@ const Card = ({ anime, ...rest }: Props) => {
   return (
     <article {...rest} className="focus-within:outline">
       <Link to={`/anime/${anime.mal_id}`} className="focus-within:outline-none">
-        <img
-          loading="lazy"
-          className="skeleton aspect-[267/475] w-full rounded-md object-cover transition-all hover:scale-105"
-          src={
-            anime.images.webp.large_image_url ||
-            anime.images.jpg.large_image_url ||
-            ""
-          }
-          alt={anime.title}
-        />
+        <div className="relative aspect-[267/475] overflow-hidden">
+          <img
+            loading="lazy"
+            className="skeleton absolute inset-0 h-full w-full rounded-md object-cover transition-all duration-300 hover:scale-110"
+            src={
+              anime.images.webp.large_image_url ||
+              anime.images.jpg.large_image_url ||
+              ""
+            }
+            alt={anime.title}
+          />
+        </div>
         {anime.score && (
-          <p className="flex items-center gap-1 text-sm text-gray-400">
+          <p className="line-2 flex items-center gap-1 text-sm text-gray-400">
             <BsStar className="text-primary" /> {anime.score}
           </p>
         )}
